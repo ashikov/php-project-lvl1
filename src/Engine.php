27c5b498e139776688c4e900a678087f806f22b3
Roman Ashikov <rmn.shkv@gmail.com>
2023-02-07 23:23:42 +0700

    fix typo

--- src/Engine.php
+++ src/Engine.php
@@ -11,7 +11,7 @@ const ROUNDS_COUNT = 3;
 
 function playGame(array $data, string $description): void
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($description);
